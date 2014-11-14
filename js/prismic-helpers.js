d65905dd35dc2c67f5dc95bc05a5b818bd5aee44
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-14 17:05:02 +0100

    add support for experiments

--- js/prismic-helpers.js
+++ js/prismic-helpers.js
@@ -26,8 +26,10 @@
           // retrieve the API
           global.Helpers.getApiHome(function(err, api, xhr) {
             if (err) { Configuration.onPrismicError(err, xhr); return; }
+            var experimentCookie = prismic.startExperiment ? prismic.utils.docCookies.getItem('io.prismic.experiment') : null;
+            var experimentRef = experimentCookie ? api.experiments.refFromCookie(experimentCookie) : null;
             var ctx = {
-              ref: (ref || api.data.master.ref),
+              ref: (ref || experimentRef || api.data.master.ref),
               api: api,
               maybeRef: (ref && ref != api.data.master.ref ? ref : ''),
               maybeRefParam: (ref && ref != api.data.master.ref ? '&ref=' + ref : ''),
@@ -69,7 +71,29 @@
         queryString: parseQS(window.location.search.substring(1)),
 
         // Hash data
-        encodedHash: parseQS(window.location.hash.substring(1))
+        encodedHash: parseQS(window.location.hash.substring(1)),
+
+        loadExperimentJs: function(googleId) {
+          if (googleId == "null") return;
+          var loadJs = function(url, callback) {
+            var head = document.getElementsByTagName('head')[0],
+                script = document.createElement('script'),
+                loaded = false;
+            script.onload = script.onreadystatechange = function() {
+              if(!loaded && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                loaded = true;
+                callback();
+                script.onload = script.onreadystatechange = null;
+                head.removeChild(script);
+              };
+            };
+            script.src = url;
+            head.appendChild(script);
+          };
+          loadJs("//www.google-analytics.com/cx/api.js?experiment=" + googleId, function() {
+            prismic.startExperiment(googleId, cxApi);
+          });
+        }
 
     };
 
